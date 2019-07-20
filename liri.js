ecb5cfa96f27d310b3e107f7c5f82f4ebd77cfcb
ginnac <ginnacampbell1990@gmail.com>
2019-07-20 17:54:18 -0600

    spotify function working good - working  on axios - omdbapi function MovieThis( )

--- liri.js
+++ liri.js
@@ -20,6 +20,7 @@ var moment = require('moment');
 //then access your keys information
 var spotify = new Spotify(keys.spotify);
 
+
 // Make it so liri.js can take in one of the following commands:
 // concert-this-------------------------------
 
@@ -58,30 +59,49 @@ axios.get(queryURL)
 //used to test the getConcert function...
 //getConcert (process.argv.slice(2).join(" "));
    
-//----------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------
 
 // spotify-this-song
     // node liri.js spotify-this-song '<song name here>' ------------------------------------------------------
     // This will show the following information about the song in your terminal/bash window
     var spotifyASong = function(nameOfTheSong){
-
+            //If no song is provided then your program will default to "The Sign" by Ace of Base.
+        if(nameOfTheSong === undefined || " "){
+            nameOfTheSong = "the sign";
+        }
      spotify.search({ type: 'track', query: nameOfTheSong })
     .then(function(response) {
-      console.log(response);
-        // Artist(s)
-        // The song's name
-        // A preview link of the song from Spotify
-        // The album that the song is from
+    
+        var items = response.tracks.items;
+        var artistsArray=[];
         //If no song is provided then your program will default to "The Sign" by Ace of Base.
 
+        //items is an array, let's get all values in items array
+        for(var i=0; i<items.length; i++){
+            
+            //getting the list of artists..
+            var artists = response.tracks.items[i].artists;
+            for(var x=0;x<artists.length;x++){
+                artistsArray.push(artists[x].name);
+            }
+            //the artistList array should be an string so we can console the list...
+            var artistsList=artistsArray.slice(0).join(", ")
+            // console logging the: Artist(s), The song's name, A preview link of the song from Spotify, The album that the song is from,
+            console.log( "\n\nArtist(s): " + artistsList + "\nSong: " + response.tracks.items[i].name + 
+            "\nLink: " + response.tracks.items[i].external_urls.spotify + 
+            "\nAlbum: " + response.tracks.items[i].album.name + "\n" );
+            
+            //emptying array so we can use it in next item from items array...
+            artistsArray = [];
+        }
     })
     .catch(function(err) {
       console.log(err);
     });
 
 }
-
-spotifyASong(process.argv.slice(2).join(" "));
+//getting terminal argument... testing spotify function
+//spotifyASong(process.argv.slice(2).join(" "));
 
         
 
@@ -106,7 +126,39 @@ spotifyASong(process.argv.slice(2).join(" "));
     // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
     // If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
     // It's on Netflix!
-    //You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
+    //You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key.
+    // You may use trilogy.
+
+
+    var movieThis = function(movieTitle){
+        var queryURL = "http://omdbapi.com/?apikey=trilogy&t=" + movieTitle + "&y=&plot=full&r=json&tomatoes=true";
+    axios.get(queryURL)
+      .then(function (response) {
+        // handle success
+        var data = response.data; 
+        console.log (data);
+        // //lets print every result inside data array....
+        // for (var concertResponses = 0; concertResponses < data.length; concertResponses ++ ){
+        // var venue = data[concertResponses].venue;
+        // // Name of the venue
+        // var venueName = venue.name;
+        // // Venue location
+        // var venueLocation = venue.city + ", " +  venue.region + ", " + venue.country; 
+        // // Date of the Event (use moment to format this as "MM/DD/YYYY")
+        // var venueTime = data[concertResponses].datetime;
+        // var venueMoment = moment(venueTime).format("MM/DD/YYYY");
+        // console.log("Venue: " + venueName + "\n Location: " + venueLocation + "\n Time: " + venueMoment + ". \n" );
+        // }
+    
+        // console.log("\n---------------------------------------------------------\n")
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+
+    movieThis(process.argv.slice(2).join(" "));
 
 // do-what-it-says
 
